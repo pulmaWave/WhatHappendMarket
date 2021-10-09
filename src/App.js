f45ad99f79d3c6bb05a900d94bbb46facefbb0da
Nguyen Cong Thinh <congthinhq@gmail.com>
2021-10-10 04:31:06 +0700

    Setup rules, variable css, folder structure, image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css'
+import React from 'react'
+import './App.scss'
+import Header from 'components/Header/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Learn React</header>
+      <Header />
     </div>
   )
 }
